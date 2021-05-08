f5f01dd78a2f83a07f3af3833cf013e3df917a6a
hassan <aymanhassan493@gmail.com>
2021-05-08 20:07:59 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,451 @@
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" /><link href="./main.css" rel="stylesheet" />
+    <title>login</title><link href="./main.css" rel="stylesheet" /><title>login</title><link href="./css/main.css" rel="stylesheet" />
+    <title>login</title><link href="./css/main.css" rel="stylesheet" /><title>login</title>
+
+</head>
+<style>
+{
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v74_2 {
+  width: 100%;
+  height: 1024px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v74_3 {
+  width: 100%;
+  height: 857px;
+  background: url("../se/v74_3.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 143px;
+  left: 97px;
+  border-top-left-radius: 124px;
+  border-top-right-radius: 124px;
+  border-bottom-left-radius: 124px;
+  border-bottom-right-radius: 124px;
+  overflow: hidden;
+}
+.v74_17 {
+  width: 163px;
+  height: 41px;
+  background: rgba(255,122,0,0.800000011920929);
+  opacity: 1;
+  position: absolute;
+  top: 776px;
+  left: 953px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+.v74_11 {
+  width: 269px;
+  height: 97px;
+  background: rgba(255,255,255,0.8999999761581421);
+  opacity: 1;
+  position: absolute;
+  top: 582px;
+  left: 980px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+.v74_4 {
+  width: 100%;
+  height: 52px;
+  background: rgba(5,43,16,0.5299999713897705);
+  opacity: 1;
+  position: absolute;
+  top: 75px;
+  left: 97px;
+}
+.v74_5 {
+  width: 217px;
+  color: url("../images/v74_5.png");
+  position: absolute;
+  top: 3px;
+  left: 97px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 36px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_6 {
+  width: 182px;
+  height: 64px;
+  background: rgba(106,31,31,0.30000001192092896);
+  opacity: 1;
+  position: absolute;
+  top: 694px;
+  left: 1035px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+.v74_7 {
+  width: 440px;
+  color: rgba(255,91,0,1);
+  position: absolute;
+  top: 684px;
+  left: 1067px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 48px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_8 {
+  width: 313px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 357px;
+  left: 992px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 48px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_9 {
+  width: 269px;
+  height: 97px;
+  background: rgba(255,255,255,0.9200000166893005);
+  opacity: 1;
+  position: absolute;
+  top: 419px;
+  left: 992px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+.v74_10 {
+  width: 225px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 521px;
+  left: 992px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 48px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_12 {
+  width: 163px;
+  height: 41px;
+  background: rgba(255,122,0,0.800000011920929);
+  opacity: 1;
+  position: absolute;
+  top: 776px;
+  left: 1130px;
+}
+.v74_13 {
+  width: 110px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 776px;
+  left: 980px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 14px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_16 {
+  width: 120px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 797px;
+  left: 1014px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 14px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_19 {
+  width: 83px;
+  color: url("../images/v74_19.png");
+  position: absolute;
+  top: 776px;
+  left: 1156px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 14px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_20 {
+  width: 77px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 799px;
+  left: 1194px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 14px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_22 {
+  width: 96px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 82px;
+  left: 176px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_23 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 84px;
+  left: 361px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_24 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 85px;
+  left: 531px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_25 {
+  width: 97px;
+  color: url("../images/v74_25.png");
+  position: absolute;
+  top: 85px;
+  left: 710px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_26 {
+  width: 159px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 82px;
+  left: 885px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_27 {
+  width: 184px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 82px;
+  left: 1134px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_28 {
+  width: 139px;
+  height: 45px;
+  background: rgba(12,121,114,1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 1046px;
+}
+.v74_29 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 1201px;
+}
+.v74_30 {
+  width: 109px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 22px;
+  left: 1232px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_31 {
+  width: 103px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 22px;
+  left: 1081px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_32 {
+  width: 504px;
+  height: 219px;
+  background: rgba(255,255,255,0.10000000149011612);
+  opacity: 1;
+  position: absolute;
+  top: 171px;
+  left: 176px;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  overflow: hidden;
+}
+.v74_33 {
+  width: 526px;
+  color: url("../images/v74_33.png");
+  position: absolute;
+  top: 188px;
+  left: 206px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 48px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_39 {
+  width: 64px;
+  height: 42px;
+  background: url("../se/v74_39.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 859px;
+  left: 1116px;
+  border-top-left-radius: 177px;
+  border-top-right-radius: 177px;
+  border-bottom-left-radius: 177px;
+  border-bottom-right-radius: 177px;
+  overflow: hidden;
+}
+.v74_38 {
+  width: 59px;
+  height: 47px;
+  background: url("../se/v74_38.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 853px;
+  left: 691px;
+  border-top-left-radius: 247px;
+  border-top-right-radius: 247px;
+  border-bottom-left-radius: 247px;
+  border-bottom-right-radius: 247px;
+  overflow: hidden;
+}
+.v74_35 {
+  width: 57px;
+  height: 42px;
+  background: url("../se/v74_35.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 853px;
+  left: 253px;
+  border-top-left-radius: 118px;
+  border-top-right-radius: 118px;
+  border-bottom-left-radius: 118px;
+  border-bottom-right-radius: 118px;
+  overflow: hidden;
+}
+.v74_40 {
+  width: 390px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 898px;
+  left: 206px;
+  font-family: Roboto;
+  font-weight: Bold;
+  font-size: 18px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_41 {
+  width: 220px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 900px;
+  left: 610px;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v74_42 {
+  width: 163px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 914px;
+  left: 1067px;
+  font-family: Roboto;
+  font-weight: Medium Italic;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+
+
+</style>
+<body>
+    <div class="v74_2"><div class="v74_3"></div>
+    <div class="v74_17"></div>
+    <div class="v74_11"></div>
+    <div class="v74_4"></div><span class="v74_5">Beauty Dent </span><div class="v74_6"></div><span class="v74_7">Login</span><span class="v74_8">Username</span><div class="v74_9"></div><span class="v74_10">Password</span><div class="v74_12"></div><span class="v74_13">No account?</span><span class="v74_16">Register now</span><span class="v74_19">Forgot your   
+              </span><span class="v74_20">Password?</span><span class="v74_22">Home</span><span class="v74_23">Services</span><span class="v74_24">Gallery</span><span class="v74_25">About </span><span class="v74_26">Appointments</span><span class="v74_27">Contact us</span><div class="v74_28"></div><div class="v74_29"></div><span class="v74_30">Sign up</span><span class="v74_31">Login</span><div class="v74_32"></div><span class="v74_33">Your smile 
+               is our priority</span><div class="v74_39"></div><div class="v74_38"></div><div class="v74_35"></div><span class="v74_40">Al Manteqah Al Oula, 
+Nasr City, Cairo Governorate
+</span><span class="v74_41">Saturday-Thursuday
+     10AM-110PM</span><span class="v74_42">0101 818 1156</span></div>
+    </body>
+</html>
