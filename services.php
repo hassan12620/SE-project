48c7004a5667463f6fb8db1f57795d578af678de
anasalshazly1 <anas.alshazly@yahoo.com>
2021-05-09 06:10:42 +0200

    e

---
+++ services.php
@@ -0,0 +1,485 @@
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Taviraj&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Arima+Madurai&display=swap" rel="stylesheet" />
+        <link href="./main.css" rel="stylesheet" />
+        <title>Services</title>    
+    </head>
+<style>
+{
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v46_0 {
+  width: 100%;
+  height: 1024px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v46_1 {
+  width: 337px;
+  height: 140px;
+  background: url("../images/v46_1.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 221px;
+  left: 98px;
+  overflow: hidden;
+}
+.v46_2 {
+  width: 166px;
+  color: rgba(5,44,17,0.6299999952316284);
+  position: absolute;
+  top: 369px;
+  left: 171px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_3 {
+  width: 122px;
+  color: rgba(5,44,17,0.6600000262260437);
+  position: absolute;
+  top: 776px;
+  left: 206px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_4 {
+  width: 187px;
+  color: rgba(5,44,17,0.46000000834465027);
+  position: absolute;
+  top: 776px;
+  left: 644px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 26px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_5 {
+  width: 300px;
+  color: rgba(5,44,17,0.46000000834465027);
+  position: absolute;
+  top: 772px;
+  left: 1056px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 26px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_6 {
+  width: 213px;
+  color: rgba(5,44,17,0.5400000214576721);
+  position: absolute;
+  top: 373px;
+  left: 649px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_7 {
+  width: 280px;
+  color: rgba(5,43,16,0.5299999713897705);
+  position: absolute;
+  top: 369px;
+  left: 1031px;
+  font-family: Roboto Condensed;
+  font-weight: Italic;
+  font-size: 30px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_8 {
+  width: 405px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 409px;
+  left: 112px;
+  font-family: Taviraj;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_9 {
+  width: 354px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 416px;
+  left: 573px;
+  font-family: Taviraj;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_10 {
+  width: 380px;
+  height: 99px;
+  background: url("../images/v46_10.png");
+  opacity: 1;
+  position: absolute;
+  top: 406px;
+  left: 554px;
+}
+.v46_11 {
+  width: 428px;
+  height: 99px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 409px;
+  left: 98px;
+}
+.v46_12 {
+  width: 333px;
+  height: 107px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 818px;
+  left: 1031px;
+}
+.v46_13 {
+  width: 382px;
+  height: 111px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 811px;
+  left: 565px;
+}
+.v46_14 {
+  width: 425px;
+  height: 111px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 811px;
+  left: 98px;
+}
+.v46_15 {
+  width: 325px;
+  height: 100px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 407px;
+  left: 1031px;
+}
+.v46_16 {
+  width: 382px;
+  height: 100px;
+  background: rgba(5,44,17,0.23999999463558197);
+  opacity: 1;
+  position: absolute;
+  top: 407px;
+  left: 565px;
+}
+.v46_17 {
+  width: 274px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 821px;
+  left: 615px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_18 {
+  width: 331px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 821px;
+  left: 1031px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_19 {
+  width: 402px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 823px;
+  left: 106px;
+  font-family: Roboto Condensed;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_20 {
+  width: 339px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 412px;
+  left: 1025px;
+  font-family: Taviraj;
+  font-weight: Regular;
+  font-size: 18px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_21 {
+  width: 306px;
+  height: 140px;
+  background: url("../images/v46_21.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 220px;
+  left: 603px;
+  overflow: hidden;
+}
+.v46_22 {
+  width: 333px;
+  height: 147px;
+  background: url("../images/v46_22.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 624px;
+  left: 98px;
+  overflow: hidden;
+}
+.v46_24 {
+  width: 334px;
+  height: 140px;
+  background: url("../images/v46_24.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 624px;
+  left: 1028px;
+  overflow: hidden;
+}
+.v46_25 {
+  width: 336px;
+  height: 149px;
+  background: url("../images/v46_25.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 624px;
+  left: 564px;
+  overflow: hidden;
+}
+.v46_26 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 14px;
+  left: 1043px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+}
+.v46_27 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 14px;
+  left: 1196px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+}
+.v46_28 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 22px;
+  left: 1058px;
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_29 {
+  width: 138px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 21px;
+  left: 1195px;
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v46_30 {
+  width: 217px;
+  color: url("../images/v46_30.png");
+  position: absolute;
+  top: 7px;
+  left: 100px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 36px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_31 {
+  width: 313px;
+  height: 141px;
+  background: url("../images/v46_31.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 217px;
+  left: 1031px;
+  overflow: hidden;
+}
+.v46_32 {
+  width: 100%;
+  height: 52px;
+  background: rgba(5,43,16,0.5299999713897705);
+  opacity: 1;
+  position: absolute;
+  top: 79px;
+  left: 98px;
+}
+.v46_33 {
+  width: 184px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 84px;
+  left: 1148px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_34 {
+  width: 159px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 87px;
+  left: 893px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_35 {
+  width: 97px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 89px;
+  left: 710px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_36 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 87px;
+  left: 531px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_37 {
+  width: 113px;
+  color: rgba(28,197,197,1);
+  position: absolute;
+  top: 88px;
+  left: 360px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v46_38 {
+  width: 115px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 88px;
+  left: 183px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+</style> 
+<body>
+<div class="v46_0"><div class="v46_1"></div>
+<span class="v46_2">Dental Filling</span>
+<span class="v46_3">Orthodontics</span>
+<span class="v46_4">Teeth Whitening</span>
+<span class="v46_5">Routine Dental & Checkup</span>
+<span class="v46_6">Tooth Extraction</span>
+<span class="v46_7">Root Canal Treatment</span>
+<span class="v46_8">By closing off spaces where bacteria can enter, a filling also helps prevent further decay. Materials used for fillings include gold,</span>
+<span class="v46_9">Tooth extraction is the removal of a tooth from its socket in the bone.</span>
+<div class="v46_10"></div><div class="v46_11"></div><div class="v46_12"></div><div class="v46_13"></div><div class="v46_14"></div><div class="v46_15"></div><div class="v46_16"></div>
+<span class="v46_17">Tooth whitening or tooth bleaching is the process of lightening the color of human teeth.,</span>
+<span class="v46_18">Regular dental exams are a critical part of preventive health care. During a dental exam, the dentist or hygienist will clean your teeth and check for cavities and gum disease</span>
+<span class="v46_19">Orthodontics is a special field of dentistry that focuses on the prevention and correction of malpositioned teeth and jaws.</span>
+<span class="v46_20">A dental extraction is the removal of teeth from the dental alveolus in the alveolar bone. </span>
+<div class="v46_21"></div><div class="v46_22"></div><div class="v46_24"></div><div class="v46_25"></div><div class="v46_26"></div><div class="v46_27"></div>
+<a href="../login.php" class="v46_28">Login</a>
+<a href="../signup.php" class="v46_29">Sign up</a>
+<span class="v46_30">Beauty Dent </span>
+<div class="v46_31"></div><div class="v46_32"></div>
+<a href="../contact-us.php" class="v46_33">Contact us</a>
+<span class="v46_34">Appointments</span>
+<span class="v46_35">About</span>
+<a href="../Gallery.php" class="v46_36">Gallery</a>
+<a href="../services.php" class="v46_37">Services</a>
+<a href="../Home.php" class="v46_38">Home</a></div>
+</body>
+</html>
