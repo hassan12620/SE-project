28810356bd686c8e07c597db744dbaa17417a873
Ahmed Amr <67699360+AhmedAmr999@users.noreply.github.com>
2021-05-09 15:57:51 +0200

    Update beautyclinic.sql

--- beautyclinic.sql
+++ beautyclinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2021 at 11:30 PM
+-- Generation Time: May 09, 2021 at 03:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -34,15 +34,17 @@ CREATE TABLE `persons` (
   `Last_Name` varchar(512) NOT NULL,
   `Type` varchar(512) NOT NULL,
   `Email` varchar(512) NOT NULL,
-  `Password` int(11) NOT NULL
+  `Password` int(11) NOT NULL,
+  `Phone` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `persons`
 --
 
-INSERT INTO `persons` (`Id`, `First_Name`, `Last_Name`, `Type`, `Email`, `Password`) VALUES
-(1, 'ahmed', 'amr', 'patient', 'ahmed@gmail.com', 12345);
+INSERT INTO `persons` (`Id`, `First_Name`, `Last_Name`, `Type`, `Email`, `Password`, `Phone`) VALUES
+(1, 'ahmed', 'amr', 'patient', 'ahmed@gmail.com', 12345, 0),
+(2, 'anas', 'ahee', 'patient', 'aly@gmail.com', 123, 1110100023);
 
 --
 -- Indexes for dumped tables
@@ -62,7 +64,7 @@ ALTER TABLE `persons`
 -- AUTO_INCREMENT for table `persons`
 --
 ALTER TABLE `persons`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
