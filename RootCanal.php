6174892af9c8e7808e9d9788d926034b2e8b39a8
hassan <aymanhassan493@gmail.com>
2021-05-09 03:54:39 +0200

    Create RootCanal.php

---
+++ RootCanal.php
@@ -0,0 +1,309 @@
+<html>
+    <head>
+    <!DOCTYPE html>
+    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" /><link href="./main.css" rel="stylesheet" /><title>Document</title><link href="./main.css" rel="stylesheet" /><title>Document</title><link href="./css/main.css" rel="stylesheet" /><title>Document</title>
+    <link href="./css/main.css" rel="stylesheet" /><title>Document</title></head>
+
+    </head>
+    <style>
+     {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v58_9 {
+  width: 100%;
+  height: 1024px;
+  background: linear-gradient(rgba(146,167,199,0.7599999904632568), rgba(196,196,196,0));
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v58_10 {
+  width: 650px;
+  height: 189px;
+  background: rgba(81,16,16,0.10000000149011612);
+  opacity: 1;
+  position: absolute;
+  top: 254px;
+  left: 92px;
+  overflow: hidden;
+}
+.v58_11 {
+  width: 655px;
+  height: 492px;
+  background: rgba(85,34,17,0.10000000149011612);
+  opacity: 1;
+  position: absolute;
+  top: 521px;
+  left: 91px;
+}
+.v58_12 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 46px;
+  left: 1016px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+}
+.v58_13 {
+  width: 626px;
+  color: rgba(6,44,17,1);
+  position: absolute;
+  top: 263px;
+  left: 94px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_14 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 46px;
+  left: 1169px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+}
+.v58_15 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 54px;
+  left: 1031px;
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v58_16 {
+  width: 138px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 53px;
+  left: 1168px;
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v58_17 {
+  width: 217px;
+  color: url("../images/v58_17.png");
+  position: absolute;
+  top: 39px;
+  left: 73px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 36px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_18 {
+  width: 100%;
+  height: 52px;
+  background: rgba(5,43,16,0.5299999713897705);
+  opacity: 1;
+  position: absolute;
+  top: 111px;
+  left: 71px;
+}
+.v58_19 {
+  width: 184px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 116px;
+  left: 1121px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_20 {
+  width: 159px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 119px;
+  left: 866px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_21 {
+  width: 97px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 121px;
+  left: 683px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_22 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 119px;
+  left: 504px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_23 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 120px;
+  left: 333px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_24 {
+  width: 115px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 120px;
+  left: 156px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_25 {
+  width: 435px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 203px;
+  left: 94px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 30px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_26 {
+  width: 581px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 470px;
+  left: 92px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 34px;
+  opacity: 1;
+  text-align: left;
+}
+.v62_2 {
+  width: 654px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 521px;
+  left: 92px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v62_4 {
+  width: 470px;
+  height: 213px;
+  background: url("../se/v62_4.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 238px;
+  left: 847px;
+  border-top-left-radius: 333px;
+  border-top-right-radius: 333px;
+  border-bottom-left-radius: 333px;
+  border-bottom-right-radius: 333px;
+  overflow: hidden;
+}
+.v62_5 {
+  width: 470px;
+  height: 212px;
+  background: url("../se/v62_5.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 496px;
+  left: 847px;
+  border-top-left-radius: 134px;
+  border-top-right-radius: 134px;
+  border-bottom-left-radius: 134px;
+  border-bottom-right-radius: 134px;
+  overflow: hidden;
+}
+.v62_6 {
+  width: 470px;
+  height: 209px;
+  background: url("../se/v62_6.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 782px;
+  left: 847px;
+  border-top-left-radius: 360px;
+  border-top-right-radius: 360px;
+  border-bottom-left-radius: 360px;
+  border-bottom-right-radius: 360px;
+  overflow: hidden;
+}
+</style>
+<body>
+<div class="v58_9"><div class="v58_10"></div><div class="v58_11"></div><div class="v58_12"></div><span class="v58_13">Root canal treatment is a treatment sequence for the infected pulp of a tooth which is intended to result in the elimination of infection and the protection of the decontaminated tooth from future microbial invasion.</span><div class="v58_14"></div><span class="v58_15">Login</span><span class="v58_16">Sign up</span><span class="v58_17">Beauty Dent </span><div class="v58_18"></div><span class="v58_19">Contact us</span><span class="v58_20">Appointments</span><span class="v58_21">About</span><span class="v58_22">Gallery</span><span class="v58_23">Services</span><span class="v58_24">Home</span><span class="v58_25">What is a Root Canal Treatment?</span><span class="v58_26">Root Canal post treatment side effects
+</span><span class="v62_2">1)Severe pain or pressure lasting more than a few days
+<br>
+<br>
+2)Visible swelling inside or outside your mouth
+<br>
+<br>
+3)An allergic reaction to medication (rash, hives or itching)
+<br>
+<br>
+4)Your bite feels uneven
+<br>
+<br>
+
+5)The temporary crown or filling, if one was put in place, comes out (losing a thin layer is normal)
+<br>
+<br>
+6)Symptoms you experienced prior to treatment return</span><div class="v62_4"></div><div class="v62_5"></div><div class="v62_6"></div></div>
+</body>
+</html>
