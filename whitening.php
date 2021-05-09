fb020b0dc06b541bf1b848887267b5f43350ed2e
moWalidz <mohammedwalid58@gmail.com>
2021-05-09 07:11:22 +0200

    SE

---
+++ whitening.php
@@ -0,0 +1,294 @@
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Arima+Madurai&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="./main.css" rel="stylesheet" />
+        <title>teeth whitening</title>
+    </head>
+    <style> 
+{
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v58_81 {
+  width: 100%;
+  height: 1024px;
+  background: linear-gradient(rgba(146,167,199,0.7599999904632568), rgba(196,196,196,0));
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v58_82 {
+  width: 650px;
+  height: 209px;
+  background: rgba(81,16,16,0.10000000149011612);
+  opacity: 1;
+  position: absolute;
+  top: 270px;
+  left: 73px;
+  overflow: hidden;
+}
+.v58_83 {
+  width: 650px;
+  height: 412px;
+  background: rgba(85,34,17,0.10000000149011612);
+  opacity: 1;
+  position: absolute;
+  top: 530px;
+  left: 74px;
+  overflow: hidden;
+}
+.v58_84 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 46px;
+  left: 1016px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+  color: rgba(255,255,255,1);
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+.v58_85 {
+  width: 590px;
+  color: rgba(6,44,17,1);
+  position: absolute;
+  top: 279px;
+  left: 85px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_86 {
+  width: 139px;
+  height: 45px;
+  background: rgba(73,199,192,1);
+  opacity: 1;
+  position: absolute;
+  top: 46px;
+  left: 1169px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+  color: rgba(255,255,255,1);
+  font-family: Roboto;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: center;
+}
+
+.v58_89 {
+  width: 217px;
+  color: url("../images/v58_89.png");
+  position: absolute;
+  top: 39px;
+  left: 73px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 36px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_90 {
+  width: 100%;
+  height: 52px;
+  background: rgba(5,43,16,0.5299999713897705);
+  opacity: 1;
+  position: absolute;
+  top: 111px;
+  left: 71px;
+}
+.v58_91 {
+  width: 184px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 116px;
+  left: 1121px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_92 {
+  width: 159px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 119px;
+  left: 866px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_93 {
+  width: 97px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 121px;
+  left: 683px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_94 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 119px;
+  left: 504px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_95 {
+  width: 113px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 120px;
+  left: 333px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_96 {
+  width: 115px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 120px;
+  left: 156px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_97 {
+  width: 334px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 211px;
+  left: 73px;
+  font-family: Roboto;
+  font-weight: Regular;
+  font-size: 30px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_98 {
+  width: 420px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 486px;
+  left: 80px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 24px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_100 {
+  width: 580px;
+  color: rgba(6,44,17,1);
+  position: absolute;
+  top: 545px;
+  left: 85px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 18px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_130 {
+  width: 588px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 708px;
+  left: 85px;
+  font-family: Arima Madurai;
+  font-weight: Medium;
+  font-size: 18px;
+  opacity: 1;
+  text-align: left;
+}
+.v58_131 {
+  width: 416px;
+  height: 215px;
+  background: url("../images/v58_131.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 246px;
+  left: 880px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+.v58_133 {
+  width: 421px;
+  height: 435px;
+  background: url("../images/v58_133.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 518px;
+  left: 884px;
+  border-top-left-radius: 40px;
+  border-top-right-radius: 40px;
+  border-bottom-left-radius: 40px;
+  border-bottom-right-radius: 40px;
+  overflow: hidden;
+}
+</style>
+<body>
+<div class="v58_81"><div class="v58_82"></div><div class="v58_83"></div>
+<span class="v58_85">tooth whitening can be a very effective way of lightening the natural colour of your teeth without removing any of the tooth surface. It cannot make a complete colour change, but it may lighten the existing shade.</span>
+<a href="../login.php" class="v58_84">Login</a>
+<a href="../signup.php" class="v58_86">Sign up</a>
+<span class="v58_89">Beauty Dent </span>
+<div class="v58_90"></div>
+<a href="../contact-us.php" class="v58_91">Contact us</a>
+<span class="v58_92">Appointments</span>
+<a href="../aboutus.php" class="v58_93">About</a>
+<a href="../Gallery.php" class="v58_94">Gallery</a>
+<a href="../services.php" class="v58_95">Services</a>
+<a href="../Home.php" class="v58_96">Home</a>
+<span class="v58_97">What is  teeth whitening?</span>
+<span class="v58_98">Why would I need my teeth whitened?</span>
+<span class="v58_100">here is a reasons why you might get your teeth whitened. Everyone is different; and just as our hair and skin colour vary, so do our teeth. Very few people have brilliant-white teeth, and our teeth can also become more discoloured as we get older.</span>
+<span class="v58_130">our teeth can also be stained on the surface by food and drinks such as tea, coffee, red wine and blackcurrant. Smoking can also stain teeth.
+
+‘Calculus' or tartar can also affect the colour of your teeth. Some people may have staining under the surface, which can be caused by certain antibiotics or by tiny cracks in the teeth which take up stains.</span><div class="v58_131"></div><div class="v58_133"></div></div>
+</body>
+</html>
