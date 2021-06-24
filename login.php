b323941e0f94d26cf1b2bb87855697398b21aaba
hassan <aymanhassan493@gmail.com>
2021-06-24 11:23:01 +0200

    g

---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+REQUIRE_ONCE "view/view.php";
+?>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- File CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>login</title>
+</head>
+
+<body>
+    <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Beauty Dent Clinic</a>
+    </header>
+
+    
+    <div class="container">
+            <div class="row py-5 mt-4 align-items-center">
+                <!-- For Demo Purpose -->
+                <div class="col-md-5 pr-lg-5 mb-5 mb-md-0">
+                    <h1>Welcome to Beauty Dent Clinic</h1>
+                    <p class="font-italic text-muted mb-0">Login with your email and password</p>
+                    </p>
+                </div>
+<?php
+$view= new view;
+$view->login();
+?>
+            </div>
+        </div>
+    </div>
+</body>
+
+    
