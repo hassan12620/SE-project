5546148e99a3d3aff95ac744fcef840deea0c6d8
Ahmed Amr <67699360+AhmedAmr999@users.noreply.github.com>
2021-06-24 11:23:48 +0200

    a

---
+++ contact-us.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+REQUIRE_ONCE "view/view.php";
+?>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="admin.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <title>All Customers</title>
+</head>
+</head>
+<body>
+    <header class="section-header">
+
+        <section class="header-main border-bottom py-2">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-2 col-4">
+                        <a href="user_home.php" class="brand-wrap">
+                        <img class="logo" src="images/logo.jpg" style = "width: 150px;height:80px;" >
+
+
+
+                        </a> <!-- brand-wrap.// -->
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <form action="#" class="search  ms-5">
+                            <div class="input-group w-100">
+                                <input type="text" class="form-control" id="search_button" placeholder="Search">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            </form> <!-- search-wrap .end// -->
+                    </div> <!-- col.// -->
+                        </div> <!-- col.// -->
+                </div> <!-- row.// -->
+            </div> <!-- container.// -->
+        </section> <!-- header-main .// -->
+    </header>
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-primary sidebar collapse ">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+
+                        <li class="nav-item  mb-2">
+                            <a class="nav-link  text-white" href="user_home".php">
+                                <i class="bi bi-calendar3"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+  <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1
+   .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+  <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 
+  1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 
+  2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+</svg>                                 Appointments
+
+                            </a>
+                        </li>
+                        <li class="nav-item  mb-2">
+                        
+                            <a class="nav-link  text-white" href="services.php">
+                                <i class="bi bi-gear-fill"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872
+   2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1
+    2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1
+     .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 
+     0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+</svg> services
+                               
+                            </a>
+                        </li>
+                        <li class="nav-item  mb-2">
+                            <a class="nav-link  text-white" href="contact-us.php">
+                                <i class="fas fa-users"></i>
+                                Contact-Us
+                            </a>
+                        </li>
+                        <li class="nav-item  mb-2">
+                            <a class="nav-link text-white" href="user-edit.php">
+                            <i class="fa fa-user"></i>
+                                edit profile    
+                            </a>
+                        </li>
+                        <li class="nav-item  mb-2">
+                            <a class="nav-link text-white" href="http://localhost/clinic/Doctors%20.php">
+                            <i class="fas fa-stethoscope"></i>                      
+                                      Doctors
+                            </a>
+                        </li>
+                        </ul>
+                    <div class="border-top border-light p-3 mb-4 mt-5">
+
+                        <div class="text-center">
+                            <a href="logout.php" class="btn btn-outline-danger">Log Out</a>
+                        </div>
+
+                    </div>
+                </div>
+            </nav>
+
+            <main class="col-md-9  ms-sm-auto col-lg-10 px-md-4 bg-light">
+            <?php
+            $view= new view;
+            $view->contactUS();
+            ?>
+
+
+            </main>
+        </div>
+    </div>
+</body>
+                  
+
